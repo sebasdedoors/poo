19d886e5d0c040db40cec7bbd6c08878e49f5bd5
sebasdedoors <sebastian28_04@hotmail.com>
2025-01-14 19:35:00 -0600

    first commit

---
+++ src/main/java/actividades/actividad1/Starter.java
@@ -0,0 +1,10 @@
+package actividades.actividad1;
+
+import edu.sebastian.rubio.actividades.actividad1.CLI;
+
+public class Starter {
+
+    public static void main(String[] args) {
+        CLI.showMenu();
+    }
+}
---
+++ src/main/java/actividades/actividad1/ui/CLI.java
@@ -0,0 +1,10 @@
+package actividades.actividad1.ui;
+
+public class CLI {
+    public static void showMenu(){
+        System.out.println("Bienvenido a la aplicación 1");
+        System.out.println("1.Crear carro");
+        System.out.println("2. Mostrar carros");
+    }
+
+}
