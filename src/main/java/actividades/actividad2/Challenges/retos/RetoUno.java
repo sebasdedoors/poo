bb1d02880092e7d6010a98b8061302508a354e0d
sebasdedoors <sebastian28_04@hotmail.com>
2025-01-21 12:17:08 -0600

    Creación de carpeta de retos y reto uno.

---
+++ src/main/java/actividades/actividad2/Challenges/retos/RetoUno.java
@@ -0,0 +1,56 @@
+package actividades.actividad2.Challenges.retos;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RetoUno { 
+    public static String reverseString(String input){ /*Es similar al metodo de burbuja en python */
+        char[] chars = input.toCharArray();
+        String reversed = "";
+        for (int i = chars.length - 1; i >= 0; i--){
+            reversed += chars[i];
+        }
+        return reversed;
+
+    }
+     public static DuplicateCharResult findFirstDuplicateChar(String input) {
+        Set<Character> seenChars = new HashSet<>();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (seenChars.contains(c)) {
+                return new DuplicateCharResult(c, i);
+            }
+            seenChars.add(c);
+        }
+        return null;
+    }
+
+    static class DuplicateCharResult{
+        char character;
+        int position;
+
+        DuplicateCharResult(char character, int position){
+            this.character = character;
+            this.position = position;
+        }
+    }
+
+    public static void main(String[] args){
+        String original = "Hola Mundo";
+        String reversed = reverseString(original);
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+
+        String testString = "abecedario";
+        DuplicateCharResult firstDuplicateResult = findFirstDuplicateChar(testString);
+        if (firstDuplicateResult != null){
+            System.out.println("Primer caracter duplicado: " + "'" + firstDuplicateResult.character + "'" + " en la posición " + firstDuplicateResult.position);
+        } else {
+            System.out.println("No hay caracteres duplicados");
+        }
+    }    
+}
+
+
+
+
--- src/main/java/actividades/actividad2/Starter.java
+++ src/main/java/actividades/actividad2/Starter.java
@@ -1,5 +1,26 @@
 package actividades.actividad2;
 
+import actividades.actividad2.ui.CLI;
+
 public class Starter {
+    public static void main(String[] args){
+        //CLI.runApp();
+        String a = "test";
+        String b = "test";
+        String b2 = new String("test"); /*El operador "new" se encarga de crear un nuevo objeto en la memoria */
+        String c = "te";
+        c += "st";
+        String d = "TEST";
+        System.out.println("a " + a);
+        System.out.println("b " + b);
+        System.out.println("c " + c);
+        System.out.println("d " + d);
+
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a == c: " + (a == c));
+        System.out.println("a equals c: " + (a.equals(c)));
+        System.out.println("a equals d: " + (a.equals(d)));
+        System.out.println("a equalsIgnoreCase d: " + (a.equalsIgnoreCase(d)));
+    }
 
 }
